78e45624ed91e31a4245d46f20b280832fa5bb44
Samarth <samarth@Samarths-MacBook-Air.local>
2026-06-20 12:07:30 +0530

    Fix CORS

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://guess-black.vercel.app"],
     credentials: true,
   }),
 );
